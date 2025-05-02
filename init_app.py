bd2341a8f1d40612c4c05e0a89c1b4e6a797d28c
Админ <test@mail.ru>
2025-05-02 14:32:57 +0300

    commit

--- init_app.py
+++ init_app.py
@@ -11,3 +11,12 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['ITEMS_IMAGES_PATH'] = 'static/images/items'
 app.config['ITEMS_FILES_PATH'] = 'static/item_files'
 app.config['USERS_AVATARS_PATH'] = 'static/images/avatars'
+
+if not os.path.exists(app.config['ITEMS_IMAGES_PATH']):
+    os.makedirs(app.config['ITEMS_IMAGES_PATH'])
+
+if not os.path.exists(app.config['ITEMS_FILES_PATH']):
+    os.makedirs(app.config['ITEMS_FILES_PATH'])
+
+if not os.path.exists(app.config['USERS_AVATARS_PATH']):
+    os.makedirs(app.config['USERS_AVATARS_PATH'])
